9e279b1ed8f404315786ff9ff1c89f3fe9d32c35
Alessio Scarfone <alessioscarfone93@gmail.com>
2022-08-10 12:39:20 +0200

    fix: remove useless file

--- src/models/GetResultResponse.ts
+++
@@ -1,9 +0,0 @@
-export default class GetResultResponse {
-    private url: string;
-    private lang: string;
-
-    constructor(url: string, lang: string) {
-        this.url = url;
-        this.lang = lang;
-    }
-}
