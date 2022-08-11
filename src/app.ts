57c6055d48a85ff56ec60fba2a200e1e9bb8c520
Alessio Scarfone <alessioscarfone93@gmail.com>
2022-08-11 22:28:29 +0200

    refactor: log setup

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import loadSwaggerPage from "./loaders/loadSwaggerPage";
 import env from "./config/env";
 import { rapidApiProxyCheckMiddleware } from "./middlewares/rapidApiProxyCheck.middleware";
 import RapidApiHeaders from "./models/RepidApiHeaders";
+import loadMorganLogger from "./loaders/loadMorganLogger";
 
 let bullMonitorConfiguredEsit = false;
 let swaggerConfiguredEsit = false;
@@ -24,21 +25,21 @@ const EXPRESS_CONTEXT_KEY = {
 
 const loadExpressMiddleware = (app: Express) => {
   loadHelmetMiddleware(app);
+  
   app.use(cors());
   app.use(compression());
   // app.use(express.text());
   app.use(express.json());
 
-  morgan.token('body', (req: Request, res: Response): string => JSON.stringify(req.body));
-  morgan.token('requestID', (req: Request, res: Response): string => req.requestID);
-  morgan.token('rapidApiUser', (req: Request, res: Response): string => req.get(RapidApiHeaders.user) || "MissingUser");
-  morgan.token('rapidApiSubscription', (req: Request, res: Response): string => req.get(RapidApiHeaders.subscription) || "MissingPlan");
-  app.use(morgan('[:date[clf]] :method :url :status [:requestID] [:rapidApiUser;:rapidApiSubscription] :response-time ms - body = :body'));
+  //Setup log
+  loadMorganLogger(app);
 
+  //Custom middlewares
   app.use(requestIDMiddleware());
   app.use(rapidApiProxyCheckMiddleware);
   app.use(errorHandlerMiddleware);
 
+  //Admin Utils
   loadMonitorPage(app)
   bullMonitorConfiguredEsit = loadBullMonitorPage(app);
   swaggerConfiguredEsit = loadSwaggerPage(app);
